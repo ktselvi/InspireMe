3ee9451258cab6cc05d737291fd12cbe42d98174
ktselvi <ktselvi@users.noreply.github.com>
2017-03-05 23:23:27 +0530

    Added Share option and FAB

--- app/src/main/java/com/ktselvi/inspireme/QuoteDetailActivity.java
+++ app/src/main/java/com/ktselvi/inspireme/QuoteDetailActivity.java
@@ -1,12 +1,18 @@
 package com.ktselvi.inspireme;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.ktselvi.inspireme.adapters.QuoteDetailPagerAdapter;
 import com.ktselvi.inspireme.model.Quote;
@@ -24,6 +30,12 @@ public class QuoteDetailActivity extends AppCompatActivity {
     @BindView(R.id.quote_detail_toolbar)
     Toolbar toolbar;
 
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+
+    private ArrayList<Quote> quotes;
+    private boolean isFav = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +45,7 @@ public class QuoteDetailActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         int currentPosition = intent.getIntExtra("QUOTE_INDEX", 0);
-        ArrayList<Quote> quotes = intent.getParcelableArrayListExtra("QUOTES_LIST");
+        quotes = intent.getParcelableArrayListExtra("QUOTES_LIST");
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(getString(R.string.app_title_quote));
@@ -44,6 +56,40 @@ public class QuoteDetailActivity extends AppCompatActivity {
         quoteDetailPager.setAdapter(pagerAdapter);
         quoteDetailPager.setCurrentItem(currentPosition);
 
+        //set the properties of FAB
+        setFAB();
+    }
+
+    private void setFAB() {
+        //Query the DB to see if this quote is marked as favourite
+        //If yes, then the FAB will be red colored, else pink
+
+        if(isFav){
+            fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.fab_background_fav)));
+        }
+
+        final Context context = this;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isFav){
+                    Toast.makeText(context, getString(R.string.fav_removed), Toast.LENGTH_SHORT).show();
+                    fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.colorAccent)));
+                    isFav = false;
+                }
+                else{
+                    Toast.makeText(context, getString(R.string.fav_added), Toast.LENGTH_SHORT).show();
+                    fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.fab_background_fav)));
+                    isFav = true;
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail_activity_menu, menu);
+        return true;
     }
 
     @Override
@@ -51,6 +97,16 @@ public class QuoteDetailActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case android.R.id.home:
                 onBackPressed();
+                return true;
+
+            case R.id.share:
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                int i = quoteDetailPager.getCurrentItem();
+                String quoteToBeShared = getString(R.string.share_quote_subject)+ " "+ quotes.get(i).getQuote()+ " - "+ quotes.get(i).getAuthor();
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, getString(R.string.share_quote_subject));
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, quoteToBeShared);
+                startActivity(Intent.createChooser(sharingIntent, "Sharing Option"));
                 return true;
         }
 
